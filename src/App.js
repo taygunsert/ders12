99869957bba92dbfce110e61a375cb954cfc49a3
Taygun Sert <taygunsert@outlook.com>
2023-04-02 17:20:02 +0300

    feat: UseEffect örneği yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [pencereBoyutu, pencereBoyutuGuncelle] = useState({
+    genislik: window.innerWidth,
+    yukseklik: window.innerHeight,
+  });
+
+  // useEffect( ()=>{ console.count("Useeffect çalıştı") } , [] )
+
+  useEffect( ()=> {
+    
+    function boyutlandirmaKodu() {
+      pencereBoyutuGuncelle({
+        genislik: window.innerWidth,
+        yukseklik: window.innerHeight,
+      });
+    }
+
+    window.addEventListener("resize", boyutlandirmaKodu);
+
+    return () => {
+      window.removeEventListener("resize", boyutlandirmaKodu);
+    };
+
+  } )
+
+
   return (
-    <p>Merhaba</p>
+    <div>
+      <p>Pencere genişliği: {pencereBoyutu.genislik}</p>
+      <p>Pencere yüksekliği: {pencereBoyutu.yukseklik}</p>
+    </div>
   );
 }
 
