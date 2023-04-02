7299f3ae09551721c73f7528f2e9d535beb5d5bf
Taygun Sert <taygunsert@outlook.com>
2023-04-02 18:12:21 +0300

    feat: useEffect saat örneği yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import Saat from "./Saat";
+
 function App() {
+  const [saatGoster, saatGosterGuncelle] = useState(true)
+
   return (
-    <p>Merhaba</p>
-  );
+    <div>
+       {saatGoster ? <Saat /> : ""}
+    </div>
+  )
 }
 
 export default App;
