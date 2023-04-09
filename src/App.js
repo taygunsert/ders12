f26bafea6aa5812e4f68ec26866d8d5af97d7918
Taygun Sert <taygunsert@outlook.com>
2023-04-09 15:24:25 +0300

    feat: useEffect dep array örneği yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [sayac, sayacGuncelle] = useState(0)
+  const [ad, adGuncelle] = useState("Ad tanımsız")
+
+  console.count("App")
+
+  useEffect( ()=>{
+    document.title = sayac
+    console.count("Use effect çalıştı")
+  }, [sayac] )
+
+  function adFonksiyonu() {
+    let zaman = new Date().getSeconds()
+    adGuncelle("Ahmet Yılmaz: " + zaman)
+  }
+
   return (
-    <p>Merhaba</p>
+    <>
+      <p>{sayac}</p>
+      <button onClick={ ()=>{  sayacGuncelle( oncekiDeger=>oncekiDeger+1)  } }>Arttır</button>
+
+      <p>Ad: {ad}</p>
+      <button onClick={adFonksiyonu}>Ad Güncelle</button>
+    </>
   );
 }
 
